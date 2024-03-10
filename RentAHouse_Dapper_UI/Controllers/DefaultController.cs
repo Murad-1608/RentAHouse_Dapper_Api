45294bc897d69504af68ffdd617aaefdddb7b02d
Murad <murad.yunus.2017@gmail.com>
2024-03-10 20:49:27 +0400

    Layout configuration in WebUI

---
+++ RentAHouse_Dapper_UI/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentAHouse_Dapper_UI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
