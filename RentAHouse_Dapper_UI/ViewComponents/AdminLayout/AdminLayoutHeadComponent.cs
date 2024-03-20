8608177e582247219f65c38b991262df2548518a
Murad <murad.yunus.2017@gmail.com>
2024-03-20 06:52:15 +0400

    Split admin panel layout to components

---
+++ RentAHouse_Dapper_UI/ViewComponents/AdminLayout/AdminLayoutHeadComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentAHouse_Dapper_UI.ViewComponents.AdminLayout
+{
+    public class AdminLayoutHeadComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
